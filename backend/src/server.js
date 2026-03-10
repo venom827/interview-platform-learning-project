c1e0ae06b7f43a99e16cbd063f83ee922cdd0e8c
venom827 <gvenom680@gmail.com>
2026-03-10 13:03:19 +0000

    done

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,6 @@ app.get("/prop",(req,res)=>{
 
 if (ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    
     app.get("/{*any}",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
